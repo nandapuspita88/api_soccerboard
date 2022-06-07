a6a9ebd4fcfef300b4585e90e94398dd60b60ffa
FlutLab.io <>
2022-06-07 03:05:45 +0000

    Initial commit

---
+++ lib/matchstat.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+Widget matchstat(String team, String logoUrl, String teamName) {
+  return Expanded(
+    child: Column(
+      children: [
+        Text(
+          team,
+          style: TextStyle(
+            fontSize: 20.0,
+          ),
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Expanded(
+          child: Image.network(
+            logoUrl,
+            width: 54.0,
+          ),
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Text(
+          teamName,
+          style: TextStyle(
+            fontSize: 18.0,
+          ),
+        ),
+      ],
+    ),
+  );
+}
